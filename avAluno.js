5f4f27950d59b9e739d5a442cf562fd7f82c5589
SILVIO WANDERSON FLORENTINO <silvio.wflorentino@senacsp.edu.br>
2023-02-03 09:46:54 -0300

    acertando erros

--- avAluno.js
+++ avAluno.js
@@ -12,13 +12,18 @@ let tAlunoRepNota = 0;
 let aluno = "";
 let tAlunoAprovado =0;
 let tAlunosReprovados = 0;
+let tMediaTurma = 0;
+let tAlunos = 0;
+let mediaMaior = 0;
+let mediaMenor = 10;
+let situacaoAluno = '';
 
 while (decisao =="1" ) {
 matricula = prompt("Digite a matricula do aluno.");
 nota1 = Number(prompt("Digite a nota do aluno."));
 nota2 = Number(prompt("Digite a nota do aluno."));
 nota3 = Number(prompt("Digite a nota do aluno."));
-frequencia = prompt("Digite a frequência do aluno.(32 máximo)");
+frequencia = Number(prompt("Digite a frequência do aluno.(32 máximo)"));
 media = (nota1 + nota2 + nota3)/3;
 
 if(frequencia >= 25){
@@ -35,16 +40,34 @@ if (media >=7) {
     tAlunoRepNota++;
 }
 if(sitFreq == 1 && sitMedia == 1){
-    situacaoAluno +=  "Aprovado";
+    situacaoAluno =  "Aprovado";
     tAlunoAprovado++;
 }else{
-    situacaoAluno += "Reprovado";
+    situacaoAluno = "Reprovado";
     tAlunosReprovados++;
 }
 
-aluno += "</br> Matricula: " +matricula + "Média: "+ media+ "Situação: " + situacaoAluno;
+tAlunos++;
+tMediaTurma += media;
+tMediaTurma = tMediaTurma /  tAlunos;
 
-document.getElementById("alunos").innerHTML = aluno;
+if (media < mediaMenor) {
+    mediaMenor = media;
+}
+
+if (media > mediaMaior) {
+    mediaMaior = media;
+}
+
+aluno += "</br> Matricula: " + matricula + " - Média: "+ media + " - Situação: " + situacaoAluno;
 
 decisao = prompt("Vc deseja cadastrar as notas dos aluno? \n 1-Sim \n 2-Não");
 }
+
+document.getElementById("alunos").innerHTML = aluno;
+document.getElementById("mediaTurma").innerHTML = tMediaTurma;
+document.getElementById("maiorMediaTurma").innerHTML = mediaMaior;
+document.getElementById("menorMediaTurma").innerHTML = mediaMenor;
+document.getElementById("totalAprovados").innerHTML = tAlunoAprovado;
+document.getElementById("totalReprovadosFalta").innerHTML = tAlunoRepFalta;
+document.getElementById("totalReprovadosNota").innerHTML = tAlunoRepNota;
